4f486037406dd43ebc903be45c62be2021683005
mickael62800 <mickael.verdin@numericable.fr>
2019-11-27 11:03:55 +0100

    controller Hopme

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @vouchers_best_marks = Voucher.where(name: "Decathlon")
+    @vouchers_top_deals = Voucher.where("SELECT * FROM vouchers
+                                        price AS p, value AS v
+                                        ORDER BY p - v ASC")
   end
 end
