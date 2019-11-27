bf958d4841d4e4105864355cc9ff4f1a05ca1206
mickael62800 <mickael.verdin@numericable.fr>
2019-11-27 15:43:25 +0100

    discount_rate

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -7,37 +7,23 @@ class Voucher < ApplicationRecord
 
   private
 
-  def day(time_in_date)
-    @in_nims = 60
-    @in_hours = 60
-    @in_days = 24
-    time_in_date / @in_nims / @in_hours / @in_days
-  end
+  def discount_rate(brand_id, end_date)
+    vouchers_count = Voucher.where(brand_id: brand_id)
 
-  def value_pourcent(date_diff, voucher_count)
-    @delta_count_voucher        = 0.35
-    @delta_price                = 0.35
-    @reduction = (((voucher_count * @delta_count_voucher) + 35) + ((date_diff * @delta_price) + 35)) / 2
-    if @reduction > 95
-      @reduction = 95
-    elsif @reduction < 35
-      @reduction = 35
+    if vouchers_count <= 100
+      coef_vouchers = 0.006 * vouchers_count + 0.35
     else
-      @reduction = @reduction
+      coef_vouchers = 0.95
     end
+
+    days_remaining = (Date.today - end_date).to_i
+    if days_remaining <= 90
+      coef_days = - 0.006 * days_remaining + 0.956
+    else
+      coef_days = 0.35
+    end
+
+    (coef_vouchers + coef_days) / 2
   end
 
-  def estimate_price(voucher_price, value_pourcent_vouchers)
-    voucher_price + value_pourcent_vouchers
-  end
-
-  def estimation
-    @voucher_name               = Voucher.brand(params[:name])
-    @voucher_expiration         = Voucher(params[:end_date])
-    @voucher_price              = Voucher(params[:price])
-    @voucher_count              = Voucher.all.brand.where(name: @voucher_name).count
-    @date_diff                  = day(@voucher_expiration - Time.now)
-    @value_pourcent_vouchers    = value_pourcent(@date_diff, @voucher_count)
-    @estime_price               = estimate_price(voucher_price, @value_pourcent_vouchers)
-  end
 end
