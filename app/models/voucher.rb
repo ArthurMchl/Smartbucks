5e0cd892297b5ef61c34235f24233032aad43179
mickael62800 <mickael.verdin@numericable.fr>
2019-11-27 15:47:15 +0100

    discount_rate

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -9,21 +9,17 @@ class Voucher < ApplicationRecord
 
   def discount_rate(brand_id, end_date)
     vouchers_count = Voucher.where(brand_id: brand_id)
-
     if vouchers_count <= 100
       coef_vouchers = 0.006 * vouchers_count + 0.35
     else
       coef_vouchers = 0.95
     end
-
     days_remaining = (Date.today - end_date).to_i
     if days_remaining <= 90
       coef_days = - 0.006 * days_remaining + 0.956
     else
       coef_days = 0.35
     end
-
     (coef_vouchers + coef_days) / 2
   end
-
 end
