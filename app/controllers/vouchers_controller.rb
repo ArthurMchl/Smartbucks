50226c9e78004760d3ffee33c29c5e3043c672e2
Werner Soudain <werner.sdn@gmail.com>
2019-12-03 18:59:17 +0100

    devise index + show

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,5 +1,5 @@
 class VouchersController < ApplicationController
-    skip_before_action :authenticate_user!, only: [:index]
+    skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     if params[:query].present? && !params[:category].empty?
