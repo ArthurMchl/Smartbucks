45ba3dabd4245a1c9e885183a4c400260a2c99da
Werner Soudain <werner.sdn@gmail.com>
2019-12-03 18:39:07 +0100

    ok

--- app/controllers/vouchers_controller.rb
+++ app/controllers/vouchers_controller.rb
@@ -1,4 +1,6 @@
 class VouchersController < ApplicationController
+    skip_before_action :authenticate_user!, only: [:index]
+
   def index
     if params[:query].present? && !params[:category].empty?
       sql_query = "brands.name ILIKE ? AND vouchers.category = ?"
