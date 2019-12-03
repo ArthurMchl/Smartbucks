72b69ca4b777662ca56c7704519de1ff0dca51cd
remi carette <carette.remi@gmail.com>
2019-12-03 19:02:26 +0100

    ok

--- app/javascript/voucher.js
+++ app/javascript/voucher.js
@@ -1,106 +1,115 @@
 
+const desactiveSubmitButton = () => {
+  const buttonValidate = document.getElementById('button-validate');
+  buttonValidate.disabled = true
+  buttonValidate.style.backgroundColor = "grey";
+}
+
+const activateSubmitButton = () => {
+  const buttonValidate = document.getElementById('button-validate');
+  buttonValidate.disabled = false;
+  buttonValidate.style.backgroundColor = "#63D699";
+}
+
+const changeInputBorderColor = (element, statut) => {
+
+  if (statut === "ok") {
+    element.style.borderColor = "#63D699"
+  } else if (statut === "forbidden") {
+    element.style.borderColor = "red";
+  } else {
+    element.style.borderColor = "#F79F1F"
+  }
+
+}
+
+const displayPriceAdvice = (inputVoucherValue, priceReductionElement) => {
+
+  const date = document.getElementById('voucher_end_date').value
+  const addTxtPriceReduction = document.getElementById("price-advice");
+  const numberVouchers = parseInt(document.getElementById('voucher_count').dataset.vc);
 
 
-<<<<<<< HEAD
+  const days = Math.ceil((new Date(date) - new Date()) / (60 * 60 * 24 * 1000));
+  let coef_vouchers = 0
+  let coef_days = 0
 
+  if(numberVouchers <= 100) {
+    coef_vouchers = 0.006 * numberVouchers + 0.35;
+  } else {
+    coef_vouchers = 0.95;
+  }
 
-    document.querySelector('.input-price').style.backgroundImage = "none";
-    const inputPrice = document.querySelector('.input-price')
-    const buttonValidate = document.getElementById('button-validate');
-    const addTxtPriceReduction = document.getElementById("price-advice");
-    let priceReduction = null
+  if (days <= 90) {
+    coef_days = 0.95 - (0.006 * days);
+  } else {
+    coef_days = 0.35;
+  }
 
-    voucher_input.addEventListener("change", (event) => {
+  let coef = (coef_vouchers + coef_days) / 2;
 
-    // recuperer la valeur du prix pour effacer si une value 'Bon' existe
-    inputPrice.value = "";
-    const date = document.getElementById('end_date').value
-    const days = Math.ceil((new Date(date) - new Date()) / (60 * 60 * 24 * 1000));
-    let coef_vouchers = 0;
-    let coef_days = 0;
+  priceReduction = (parseInt(inputVoucherValue.value) * coef);
+  priceReduction = Math.ceil(priceReduction)
+  addTxtPriceReduction.innerHTML = `Prix de vente conseillé : <span class="weight-font">${Math.ceil(priceReduction)} €</span>`;
 
-      if(vc <= 100) {
-        coef_vouchers = 0.006 * vc + 0.35;
-      } else {
-        coef_vouchers = 0.95;
-      }
-=======
+  priceReductionElement.dataset.price = priceReduction
+}
+
 const formNewVoucher = document.getElementById('new_voucher');
 
 if (formNewVoucher) {
 
-  const voucher_input = document.getElementById('voucher_value');
-  const vc = parseInt(document.getElementById('voucher_count').dataset.vc);
+  const inputPrice = document.getElementById('voucher_price')
+  const buttonValidate = document.getElementById('button-validate');
+  const inputVoucherValue = document.getElementById('voucher_value');
+  const priceReductionElement = document.getElementById('price-reduction');
 
-  voucher_input.addEventListener("blur", (event) => {
-    const date = document.getElementById('voucher_end_date').value
-    const days = Math.ceil((new Date(date) - new Date()) / (60 * 60 * 24 * 1000));
-    let coef_vouchers = 0
-    let coef_days = 0
-    if(vc <= 100) {
-      coef_vouchers = 0.006 * vc + 0.35;
-    } else {
-      coef_vouchers = 0.95;
+  let priceReduction = null
+
+  inputVoucherValue.addEventListener("change", (event) => {
+    displayPriceAdvice(inputVoucherValue, priceReductionElement);
+  })
+
+
+
+  inputPrice.addEventListener("input", (event) => {
+    priceReduction = parseInt(priceReductionElement.dataset.price, 10)
+    let alertPrice = document.getElementById("price-alert")
+    const price = parseInt(inputPrice.value, 10)
+    const value = parseInt(inputVoucherValue.value, 10)
+
+    if(price > value) {
+      desactiveSubmitButton();
+      changeInputBorderColor(event.currentTarget, "forbidden");
+      alertPrice.innerHTML = "Le prix indiqué est supérieur à la valeur du bon";
+      alertPrice.style.color = "#EA2027"
+
+    } else if (priceReduction > value)  {
+      activateSubmitButton();
+      changeInputBorderColor(event.currentTarget, "");
+      alertPrice.innerHTML = "";
+
+    } else if(priceReduction === value) {
+      activateSubmitButton();
+      changeInputBorderColor(event.currentTarget, "");
+      alertPrice.innerHTML = "";
+
+    } else if(price === value) {
+      activateSubmitButton();
+      changeInputBorderColor(event.currentTarget, "");
+      alertPrice.innerHTML = "";
+
+    } else if(priceReduction < price && price < value) {
+      activateSubmitButton();
+      changeInputBorderColor(event.currentTarget, "");
+      alertPrice.innerHTML = "";
+
+    } else if(price < priceReduction) {
+      activateSubmitButton();
+      changeInputBorderColor(event.currentTarget, "ok");
+      alertPrice.innerHTML = ""
     }
-
-    if (days <= 90) {
-      coef_days = 0.95 - (0.006 * days);
-    } else {
-      coef_days = 0.35;
-    }
->>>>>>> c966407aca573c6216ed3422230e7c694c430346
-
-      if (days <= 90) {
-        coef_days = 0.95 - (0.006 * days);
-      } else {
-        coef_days = 0.35;
-      }
-    let coef = (coef_vouchers + coef_days) / 2;
-
-    priceReduction = (parseInt(voucher_input.value) * coef);
-    priceReduction = Math.ceil(priceReduction)
-    addTxtPriceReduction.innerHTML = `Prix de vente conseillé : <span class="weight-font">${Math.ceil(priceReduction)} €</span>`;
-    })
-
-   inputPrice.addEventListener("change", (event) => {
-
-      let alertPrice = document.getElementById("price-alert")
-
-      if(parseInt(inputPrice.value) > parseInt(voucher_input.value)  ) {
-
-        scssStyle(event, buttonValidate, "#EA2027", "#7f8c8d", true)
-
-        alertPrice.innerHTML = "Le prix indiqué est supérieur à la valuer du bon";
-        alertPrice.style.color = "#EA2027"
-
-      } else if (parseInt(priceReduction) > parseInt(inputPrice.value))  {
-
-        scssStyle(event, buttonValidate, "#C4E538", "black", false)
-        alertPrice.innerHTML = "";
-
-      } else if(parseInt(priceReduction) === parseInt(inputPrice.value)) {
-
-        scssStyle(event, buttonValidate, "#006266", "black", false)
-        alertPrice.innerHTML = "";
-
-      } else if(parseInt(inputPrice.value) === parseInt(voucher_input.value)) {
-
-        scssStyle(event, buttonValidate, "#F79F1F", "black", false)
-        alertPrice.innerHTML = "";
-
-      } else if(parseInt(priceReduction) < parseInt(inputPrice.value) && parseInt(inputPrice.value) < parseInt(voucher_input.value)) {
-
-        scssStyle(event, buttonValidate, "#FFC312", "black", false)
-        alertPrice.innerHTML = ""
-
-      }
-    });
-
-   let scssStyle = (event, buttonValidate, colorBoder, colorBack, validation) => {
-        event.currentTarget.style.borderColor = colorBoder;
-        buttonValidate.style.backgroundColor = colorBack;
-        buttonValidate.disabled = validation;
-  }
+  });
 }
 
 
