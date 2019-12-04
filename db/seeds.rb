478a910d7cd10ce7e1069bb6089a9c9b919ebdcb
Werner Soudain <werner.sdn@gmail.com>
2019-12-04 15:07:34 +0100

    added destroy to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 puts '---- Delete all items in database ----'
+Notification.destroy_all
 Order.destroy_all
 Voucher.destroy_all
 Brand.destroy_all
