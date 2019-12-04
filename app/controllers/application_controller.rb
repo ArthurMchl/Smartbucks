781cc2fa94f9223f951463b13befb9a55c8145d4
Alexandre Bouvier <alexandre.b2506@gmail.com>
2019-12-04 12:11:37 +0100

    WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,12 +14,8 @@ class ApplicationController < ActionController::Base
   # end
 
   def notification
-    notifications = Notification.where("user_id = ? AND viewed = ?", current_user, false)
-   # Si j qi des notifications qlors j affiche la flash notice
-  #tu passes toutes les modifq true
-
+    notifications  = Notification.where("user_id = ? AND viewed = ?", current_user, false)
     flash[:notice] = "Vous avez vendu #{notifications.size} bon(s)" if notifications.size > 0
-
     notifications.each do |notification|
       notification.viewed = "true"
       notification.save
